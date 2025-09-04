9853a341289f53530e8ee0c749df9b637db387af
Evan <evanh1393@gmail.com>
2025-09-04 17:57:27 -0400

    ghostty-and-nvim-stuff

--- nvim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -133,3 +133,14 @@ lspconfig.fish_lsp.setup {
     capabilities = capabilities,
     filetypes = { "fish" },
 }
+
+-- Force Go files to use 4-wide real tabs
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "go",
+    callback = function()
+        vim.opt_local.expandtab = false -- real tabs, not spaces
+        vim.opt_local.tabstop = 4 -- tab displays as 4 spaces wide
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.softtabstop = 4
+    end,
+})
