8eb66bf6961adc79f62d9080d8af34d01eb75ac1
Evan Hoefling <evanh@evan-dekstop.mynetworksettings.com>
2025-09-17 15:15:51 -0400

    everfores-stuff

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,15 +1,14 @@
-return {
-	-- Your existing configuration
-	color_scheme_dirs = { "~/.config/wezterm/colors" },
-	color_scheme = "Everforest Dark (Medium)",
-	
-	-- Disable window decorations (title bar, borders, etc.)
-	window_decorations = "NONE",
-	
-	-- Hide tab bar when there's only one tab
-	hide_tab_bar_if_only_one_tab = true,
-	
-	-- Or completely disable the tab bar
-	-- enable_tab_bar = false,
-	
-}
+local wezterm = require("wezterm")
+
+local config = {}
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme_dirs = { "~/.config/wezterm/colors" }
+config.color_scheme = "Everforest Dark (Medium)"
+config.window_decorations = "NONE"
+config.enable_tab_bar = false
+config.enable_wayland = true 
+
+return config
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,1 +1,4 @@
 eval "$(starship init zsh)"
+
+eval "$(zoxide init zsh)"
+export PATH="$HOME/.local/bin:$PATH"
