bee40828b22a0e9d793747d858af496b27feda5b
Evan Hoefling <evanh1393@gmail.com>
2025-09-19 20:15:16 -0400

    config-changes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,13 +2,13 @@ local wezterm = require("wezterm")
 
 local config = {}
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.color_scheme_dirs = { "~/.config/wezterm/colors" }
 config.color_scheme = "Everforest Dark (Medium)"
 config.window_decorations = "NONE"
 config.enable_tab_bar = false
-config.enable_wayland = true 
+config.enable_wayland = true
 
 return config
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -9,11 +9,10 @@ export PATH="$HOME/.local/bin:$PATH"
 # bun completions
 [ -s "/home/evanh/.bun/_bun" ] && source "/home/evanh/.bun/_bun"
 
-# bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
-
 export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.aws/scripts:$PATH"
 
 if [ -f ~/.zsh_aliases ]; then
     source ~/.zsh_aliases
